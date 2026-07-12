af4ed9844fe3e8978455a3e312cf5c78061ff406
Fennex <mifdhalwajdi@gmail.com>
2026-07-12 20:40:45 +0800

    Update main.py

--- AccRegisterV1/main.py
+++ AccRegisterV1/main.py
@@ -1,4 +1,5 @@
 from utils import wait, cls, divider
+from pages import signup_page, login_page
 
 # Configures
 AppsName: str = "FENNEX.CO"
@@ -7,86 +8,28 @@ AppsName: str = "FENNEX.CO"
 DataStore: dict = {}
 
 
-## Pages
-# Sign-up
-def signup_page():
-    cls()
-    divider()
-    print("      Make a new fresh account!")
-    divider()
-    print("Type 'Back' at any prompt to return\n")
-
-    sNameInput = input("Enter your username: \n").strip()
-    if sNameInput.lower() == "back":
-        cls()
-        return
-
-    sPassInput = input("Enter your passwords: \n").strip()
-    if sPassInput.lower() == "back":
-        cls()
-        return
-
-    if sNameInput in DataStore:
-        print("[FAILED] Username already taken!")
-    else:
-        DataStore[sNameInput] = sPassInput
-        print("[SUCCESS] Account created successfully!")
-        
-    wait(3)
-    cls()
-
-
-# Log-in
-def login_page():
-    cls()
-    divider()
-    print("      Already have an account?")
-    divider()
-    print("Type 'Back' at any prompt to return\n")
-
-    lNameInput = input("Enter your username: \n").strip()
-    if lNameInput.lower() == "back":
-        cls()
-        return
-
-    lPassInput = input("Enter your passwords: \n").strip()
-    if lPassInput.lower() == "back":
-        cls()
-        return
-
-    if lNameInput in DataStore and lPassInput == DataStore[lNameInput]:
-        print(f"[SUCCESS] Welcome aboard captain! {lNameInput}")
-    else:
-        print(
-            "[FAILED] Unavaiable to get into your's account! \n Usual reason: \n Wrong username \n Wrong passwords \n Account didnt exist \n Data Corrupted"
-        )
-
-    wait(3)
-    cls()
-
-
 cls()  # clear the terminal first for clean output.
 
 while True:
-    divider()
+    divider(36)
     print(f"\tWELCOME TO {AppsName}")
-    divider()
+    divider(36)
     print("""Choose the number to go to the page.
       
    1.  Sign-Up
    2.  Log-In
    3.  Exit
       """)
-    divider()
+    divider(36)
 
     Userpage_Input = input("Choose the page number: ")
     # print(f"User choose number: {Userpage_Input}") # Debugging
 
     ## Pages Shifter Logic (HardCoded)
     if Userpage_Input == "1":
-        signup_page()
+        signup_page(DataStore)
     elif Userpage_Input == "2":
-        login_page()
+        login_page(DataStore)
     elif Userpage_Input == "3":
         cls()
         print("Thanks for using our service.")
